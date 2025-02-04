717571a5fd6bc847584d37ec09a96a80aa1a25ad
toms74209200 <motomotomato@gmail.com>
2025-02-04 21:04:07 +0900

    :hammer: Update container settings for manual test

---
+++ manualtest/install.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+function setup_openapi_generator() {
+    OPENAPI_GENERATOR_CLI_INSTALLED=$(openapi-generator-cli version >/dev/null 2>&1 && echo true || echo false)
+    if [ "$OPENAPI_GENERATOR_CLI_INSTALLED" = true ]; then
+        return
+    fi
+
+    sudo apt-get update
+    sudo apt-get install -y openjdk-17-jdk nodejs npm
+    npm install -g @openapitools/openapi-generator-cli
+}
+
+function setup_openapi_gen() {
+    if [ -d openapi_gen ]; then
+        return
+    fi
+
+    setup_openapi_generator
+    openapi-generator-cli generate -i https://raw.githubusercontent.com/toms74209200/openapi-todo-example/refs/heads/master/reference/spec.yaml -g python -o openapi_gen
+}
+
+setup_openapi_gen
+
+pip install -r requirements.txt
+pip install -r openapi_gen/requirements.txt
+pip install setuptools
+cd openapi_gen && sudo python setup.py install --user
